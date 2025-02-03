66408a4686b625532b81630613d1c6cc93396471
ChefTandy17 <ttandy@ucsc.edu>
2025-02-03 15:30:04 -0800

    updating my progression in main.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
 //Name: Tyvin Tandy
-//Title: Endless Runner Name Placeholder
-//Approx Hours: 6 hour(s)
+//Title: Streamer Driver
+//Approx Hours: 12 hour(s)
 //Creative Tilt: There is nothing program interesting... yet.
 //There is nothing visually interesting... yet
 
 // main game object for Phaser
 
 //Things to do
-//Properly transition between Scenes and allow the player to restart w/out having to reload the page (1) 
-//Implement proper collision detection (via Arcade Physics or a custom routine) (1)
-//Use a minimum of four sound effects for key mechanics, UI, and/or significant events appropriate to your game design (1)
-//Use randomness to generate escalating challenge, e.g. terrain, pickups, etc. (1)
-//Include some metric of accomplishment that a player can improve over time, e.g., score, survival time, etc. (1)
-//Run without significant crashes or errors (1)
-//Include in-game credits for all roles, assets, music, etc. (1)
+//Properly transition between Scenes and allow the player to restart w/out having to reload the page (1) //hazard bugs
+//Implement proper collision detection (via Arcade Physics or a custom routine) (1)                      //collision, but velocity doesn't stop or is detected
+//Use a minimum of four sound effects for key mechanics, UI, and/or significant events appropriate to your game design (1)   //in progress    
+//Run without significant crashes or errors (1)                                                         //in progress
+//Include in-game credits for all roles, assets, music, etc. (1)                                        //in progress
 
 let config = {
     type: Phaser.AUTO,
